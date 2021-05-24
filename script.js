3138a52c74cabb8e293d4e7465aa2f119709313d
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-05-23 19:31:11 -0600

    background color feature completed

--- script.js
+++ script.js
@@ -5,28 +5,40 @@ const saveBtn = $('.saveBtn');
 //display current time on page
 function updateTime(){
     var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-   $('#currentDay').html( currentTime );
+    $('#currentDay').html( currentTime );
 };
 
 //keep time current by updating by one second
-updateTime();
 setInterval(function(){
    updateTime();
 }, 1000);
 
-//if current time is less than the time block, change color of div to grey
-if (updateTime() < $("#hour").text) {
-    $(".time-block").addClass("past")
-} 
-    //if current time equal to the time block, change color of div to red
-    else if (updateTime() == $("#hour".text)) {
-    $(".time-block").addClass("present")
-}
-    //if current time is greater than time block, change color of div to green
-    else {
-    $(".time-block").addClass("future")
+
+// function to set background colors for divs, depending on if they're past/present/future
+function setBgColors() {
+    //sets currentHour = the current hour, from moment.js
+    const currentHour = parseInt(moment().format("H"));
+    
+
+    $(".description").each(function () {
+        //gives elHour (the hour pulled from parseInt) an ID
+        var elHour = parseInt($(this).attr("id"));
+        console.log($(this));
+        
+        //changes description box depending if the time slot is past/present/future
+        if (elHour < currentHour) {
+            $(".time-block").addClass("past")
+        } else if (elHour > currentHour) {
+            $(".time-block").addClass("future")
+        } else {
+          $(".time-block").addClass("present")
+        }
+    });
 }
 
+setBgColors();
+
+//function that saves items to localstorage
 $(".saveBtn").on("click", function(){
     console.log($(this))
     var textValue = $(this).siblings(".description").val();
@@ -36,17 +48,13 @@ $(".saveBtn").on("click", function(){
 });
 
 
-
-$(".9am").val(localStorage.getItem("9am"));
-$(".10am").val(localStorage.getItem("10am"));
-$(".11am").val(localStorage.getItem("11am"));
-$(".12pm").val(localStorage.getItem("12pm"));
-$(".1pm").val(localStorage.getItem("1pm"));
-$(".2pm").val(localStorage.getItem("2pm"));
-$(".3pm").val(localStorage.getItem("3pm"));
-$(".4pm").val(localStorage.getItem("4pm"));
-$(".5pm").val(localStorage.getItem("5pm"));
-$(".9am").val(localStorage.getItem("9am"));
-
-// for each time value item , get the text value from local storage
-
+// displays localstorage on refresh
+$("#9").val(localStorage.getItem("9am"));
+$("#10").val(localStorage.getItem("10am"));
+$("#11").val(localStorage.getItem("11am"));
+$("#12").val(localStorage.getItem("12pm"));
+$("#13").val(localStorage.getItem("1pm"));
+$("#14").val(localStorage.getItem("2pm"));
+$("#15").val(localStorage.getItem("3pm"));
+$("#16").val(localStorage.getItem("4pm"));
+$("#17").val(localStorage.getItem("5pm"));
